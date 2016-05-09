dd7ec2e6158590969ba9479c047ac16e7fcea6ac
cmtp <christian.tola@hotmail.com>
2016-05-09 14:13:27 -0400

    changes ui example

--- generators/app/templates/app/js/controllers/user.controller.js
+++ generators/app/templates/app/js/controllers/user.controller.js
@@ -10,9 +10,22 @@
     function userController($http) {
         //vars
         var vm = this;
-        vm.user = {
+        vm.users = [
+        {
+            id: 1,
           name: "chris",
           last: "tola"
+        },
+        {
+            id: 2,
+            name: "juan",
+            last: "Perez"
+        },
+        {
+            id: 3,
+            name: "deep",
+            last: "blue"
         }
+        ]
     }
 })();

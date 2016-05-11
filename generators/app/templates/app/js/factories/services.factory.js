71796823ac61d5ade6df451b62df80513861c081
cmtp <christian.tola@hotmail.com>
2016-05-11 17:20:10 -0400

    correccion service

--- generators/app/templates/app/js/factories/services.factory.js
+++ generators/app/templates/app/js/factories/services.factory.js
@@ -6,8 +6,8 @@
     .factory('servicesFactory', ['$resource', servicesFactory]);
 
   function servicesFactory($resource) {
-    return $resource(':resourceName',{},{
-      query: {method: 'get', params:{resourceName:'http://api.randomuser.me/?page=1&results=10'}}
+    return $resource('http://api.randomuser.me/?page=1&results=10',{},{
+      query: {method: 'get', params:{}}
     });
   }
 })();

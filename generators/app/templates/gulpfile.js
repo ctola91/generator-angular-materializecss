6e5acb4d44c4f8e2d813ea901bad9b9929723191
cmtp <christian.tola@hotmail.com>
2016-06-10 13:58:38 -0400

    add minify css html and js

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -6,7 +6,11 @@ var gulp = require("gulp"),
     historyApiFallback = require("connect-history-api-fallback"),
     sass = require("gulp-sass"),
     concat = require('gulp-concat'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps')
+    cleanCSS = require('gulp-clean-css')
+    uglify = require('gulp-uglify')
+    htmlmin = require('gulp-htmlmin')
+    mainBowerFiles = require('main-bower-files');
 
 // 1. Servidor web de desarrollo
 gulp.task('server', function() {
@@ -45,12 +49,34 @@ gulp.task('html', function() {
     gulp.src('./app/**/*.html')
         .pipe(connect.reload());
 });
-gulp.task('minified', function() {
+/*minificar html*/
+gulp.task('minify-html', function() {
+    return gulp.src('./app/**/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'))
+});
+
+/* minificar y encriptar js */
+gulp.task('minify-js', function() {
     return gulp.src('./app/js/**/*.js')
         .pipe(sourcemaps.init())
         .pipe(concat('app.min.js'))
+        .pipe(uglify())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./app/dist'));
+        .pipe(gulp.dest('./dist/js'));
+});
+/* Minificar css */
+gulp.task('minify-css', function() {
+    return gulp.src('./app/css/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css'));
+});
+/*move bower components to build project*/
+gulp.task('build-bower', function() {
+    return gulp.src(mainBowerFiles(), { base: '.app/bower_components' })
+    .pipe(gulp.dest('./dist/bower_components'));
 });
 /*inspector que escucha los cambios en los css y html*/
 gulp.task('watch', function() {
@@ -59,6 +85,11 @@ gulp.task('watch', function() {
     gulp.watch('./app/sass/**/*.scss', ['sass']);
     gulp.watch(['./app/css/**/*.css'], ['css']);
 });
+/* Compilar y minificar archivos para colocar en produccion */
+gulp.task('build',
+          ['minify-html', 'minify-css', 'minify-js']
+         );
+
 /*tareas por defecto*/
 gulp.task('default',
     ['server', 'watch']
